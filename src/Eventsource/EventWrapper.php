64c280c083ff25358904f74985d96bb6c91ec298
Tim Lytle <tim@timlytle.net>
2025-05-14 15:39:25 -0400

    Updated serde.

--- src/Eventsource/EventWrapper.php
+++ src/Eventsource/EventWrapper.php
@@ -2,6 +2,10 @@
 
 namespace BattleSnake\Eventsource;
 
+use BattleSnake\Event\End;
+use BattleSnake\Event\Move;
+use BattleSnake\Event\Start;
+use BattleSnake\Event\Turn;
 use BattleSnake\Tests\Unit\Event\EventFixture;
 use Crell\Serde\Attributes\StaticTypeMap;
 
@@ -10,6 +14,9 @@ readonly class EventWrapper
     public function __construct(
         #[StaticTypeMap(key: 'type', map: [
             'test' => EventFixture::class,
+            'start' => Start::class,
+            'end' => End::class,
+            'turn' => Turn::class,
         ])]
         public Event $event,
     ){
