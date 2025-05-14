0291677cac69696266f62509268fd65174c3bf0b
Tim Lytle <tim@timlytle.net>
2025-05-14 16:24:45 -0400

    Aggregate Root TDD

---
+++ test/Unit/Root/GameTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace BattleSnake\Tests\Unit\Root;
+
+use BattleSnake\Domain\GameState;
+use BattleSnake\Domain\Parser\GameStateParserFactory;
+use BattleSnake\Eventsource\Event;
+use BattleSnake\Root\Game as SUT;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class GameTest extends TestCase
+{
+    #[Test]
+    #[DataProvider('provideEventsAndAssertion')]
+    public function can_rebuild_from_past_events(callable $assertion, Event ...$events): void
+    {
+        $uuid = Uuid::uuid4();
+        $game = new SUT($uuid);
+        $game->loadEvents(...$events);
+
+        $assertion($game);
+    }
+
+    #[Test]
+    public function can_add_new_events(): void
+    {
+
+    }
+
+    #[Test]
+    public function can_get_events_not_persisted(): void
+    {
+
+    }
+
+    public static function provideEventsAndAssertion(): \Generator
+    {
+        $events = [
+            new \BattleSnake\Event\Start(self::getJsonData('four-player-large-start')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move1')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move2')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move3')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move4')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move5')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move6')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move7')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move8')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move9')),
+            new \BattleSnake\Event\Turn(self::getJsonData('four-player-large-move10')),
+            new \BattleSnake\Event\End(self::getJsonData('four-player-large-end')),
+        ];
+
+        yield [
+            function (SUT $game) {
+                TestCase::assertFalse($game->isFinished());
+                TestCase::assertFalse($game->isStarted());
+            },
+            $events[0]
+        ];
+
+        yield [
+            function (SUT $game) {
+                TestCase::assertFalse($game->isFinished());
+                TestCase::assertTrue($game->isStarted());
+                TestCase::assertSame(0, $game->getTurn());
+            },
+            $events[0],
+            $events[1],
+        ];
+
+        yield [
+            function (SUT $game) {
+                TestCase::assertFalse($game->isFinished());
+                TestCase::assertTrue($game->isStarted());
+                TestCase::assertSame(2, $game->getTurn());
+            },
+            $events[0],
+            $events[1],
+            $events[2],
+            $events[3],
+        ];
+
+
+        yield [
+           function (SUT $game) {
+                TestCase::assertTrue($game->isFinished());
+                TestCase::assertTrue($game->isStarted());
+                TestCase::assertSame(9, $game->getTurn());
+            },
+            ...$events
+        ];
+    }
+
+    private static function getJsonData(string $string): GameState
+    {
+        $parser = GameStateParserFactory::make();
+        $json = file_get_contents(__DIR__ . '/../../requests/' . $string . '.json');
+        if ($json === false) {
+            throw new \RuntimeException('Failed to read JSON file');
+        }
+        $data = json_decode($json, true);
+        if ($data === null) {
+            throw new \RuntimeException('Failed to decode JSON data');
+        }
+        return $parser->parse($data);
+    }
+}
